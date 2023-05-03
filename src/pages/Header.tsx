5c82f30278b82bc0bb2a970627bfad33aadcd044
verve-neowise <verve.neowise@gmail.com>
2023-05-03 11:48:38 +0500

    add few projects

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 function Header() {
   const [menu, setMenu] = useState(true);
   return (
-    <header className="fixed top-0 inset-x-0 bg-black/50 backdrop-blur-sm">
+    <header className="fixed top-0 inset-x-0 bg-black/50 backdrop-blur-sm  border-green-400/20">
       <div className="container mx-auto px-4 md:px-0 py-5 flex justify-between items-center">
         <h2 className="text-2xl text-center md:text-left">
           <span className="font-bold text-green-400">Android&nbsp;</span>
@@ -19,10 +19,10 @@ function Header() {
         </button>
         <div
           className={classNames(
-              "fixed md:relative h-screen md:h-auto p-4 md:p-0 text-lg md:text-base top-0 right-0 w-full max-w-xs md:w-auto md:max-w-none bg-black md:bg-transparent transition-all",
-              {
-                  "translate-x-full md:translate-x-0": menu
-              }
+            "fixed md:relative h-screen md:h-auto p-4 md:p-0 text-lg md:text-base top-0 right-0 w-full max-w-xs md:w-auto md:max-w-none bg-black/50 backdrop-blur-sm md:bg-transparent transition-all",
+            {
+              " translate-x-full md:translate-x-0 backdrop-blur-sm": menu
+            }
           )}
         >
           <ul className="md:flex">
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,22 +1,48 @@
-import React from 'react'
 import kinoCat from '../assets/projects/KinoCat.png'
 import spic from '../assets/projects/Spic.png'
 import tracko from '../assets/projects/Tracko.png'
 import midapp from '../assets/projects/MidApp.png'
 import github from '../assets/github.svg'
 import play from '../assets/play.png'
+import uzmega from '../assets/projects/Uzmega.png'
+import mbos from '../assets/projects/MBos.png'
+import tasks from '../assets/projects/Taskie.png'
+
 import Section from './Section'
 
 function Projects() {
     return (
         <Section title='Projects' anchor='projects'>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-16 '>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-10 '>
+
+                <App
+                    name='Taskie'
+                    image={tasks}
+                    instrumentals={['Kotlin', 'Android', 'Jetpack Compose', 'Jetpack Components', 'FCM', 'Retrofit2', 'Picasso', 'MVVM', 'Koin', 'Kotlin Coroutines']}
+                    desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo illo, autem officia tenetur nam suscipit rerum deleniti quae dolorem animi at incidunt eveniet! Praesentium repellendus, accusantium illo doloremque eos accusamus."
+                />
+
+                <App
+                    name='MBos'
+                    image={mbos}
+                    playPage='https://play.google.com/store/apps/details?id=com.umega&hl=ru&gl=US'
+                    instrumentals={['Kotlin', 'Android', 'FCM', 'Retrofit2', 'Picasso', 'MVVM', 'Dagger Hilt', 'Kotlin Coroutines']}
+                    desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo illo, autem officia tenetur nam suscipit rerum deleniti quae dolorem animi at incidunt eveniet! Praesentium repellendus, accusantium illo doloremque eos accusamus."
+                />
+
+                <App
+                    name='UzMega'
+                    image={uzmega}
+                    playPage='https://play.google.com/store/apps/details?id=com.umega&hl=ru&gl=US'
+                    instrumentals={['Kotlin', 'Android', 'Firebase Auth', 'FCM', 'Retrofit2', 'MVVM', 'Kotlin Coroutines', 'Node.js', 'PostgreSQL']}
+                    desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo illo, autem officia tenetur nam suscipit rerum deleniti quae dolorem animi at incidunt eveniet! Praesentium repellendus, accusantium illo doloremque eos accusamus."
+                />
 
                 <App
                     name='Tracko'
                     image={tracko}
                     source='source'
-                    playPage='play-page'
+                    instrumentals={['Kotlin', 'Android', 'Firebase Storage', 'Retrofit2', 'MVVM', 'Goolgle Maps', 'Node.js', 'PostgreSQL']}
                     desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo illo, autem officia tenetur nam suscipit rerum deleniti quae dolorem animi at incidunt eveniet! Praesentium repellendus, accusantium illo doloremque eos accusamus."
                 />
 
@@ -24,7 +50,7 @@ function Projects() {
                     name='Spic'
                     image={spic}
                     source='source'
-                    playPage='play-page'
+                    instrumentals={['Kotlin', 'Android', 'Firebase Storage', 'Coroutines', 'OkHttp', 'Node.js', 'PostgreSQL']}
                     desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo illo, autem officia tenetur nam suscipit rerum deleniti quae dolorem animi at incidunt eveniet! Praesentium repellendus, accusantium illo doloremque eos accusamus."
                 />
 
@@ -32,7 +58,6 @@ function Projects() {
                     name='Kino Cat'
                     image={kinoCat}
                     source='source'
-                    playPage='play-page'
                     desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo illo, autem officia tenetur nam suscipit rerum deleniti quae dolorem animi at incidunt eveniet! Praesentium repellendus, accusantium illo doloremque eos accusamus."
                 />
 
@@ -41,7 +66,7 @@ function Projects() {
                     name='Mid App'
                     image={midapp}
                     source='source'
-                    playPage='play-page'
+                    instrumentals={['Java', 'Spring', 'Hibernate', 'JavaFX']}
                     desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo illo, autem officia tenetur nam suscipit rerum deleniti quae dolorem animi at incidunt eveniet! Praesentium repellendus, accusantium illo doloremque eos accusamus."
                 />
             </div>
@@ -53,8 +78,8 @@ function Projects() {
 
 
 function App(
-    { name, image, source, playPage, desc }:
-        { name: string, image: string, source: string, playPage: string, desc: string }
+    { name, image, source, playPage, desc, instrumentals }:
+        { name: string, image: string, source?: string, playPage?: string, desc: string, instrumentals?: string[] }
 ) {
     return (
         <div className='flex flex-col gap-3 w-full'>
@@ -62,19 +87,34 @@ function App(
             <div className="flex justify-between items-center">
                 <h3 className='text-2xl font-semibold'> {name} </h3>
                 <div className="flex gap-2 text-sm">
-                    <a className='p-2 border border-white/20 rounded-md flex gap-2 items-center ' href={source}>
-                        <img src={github} width='16px' alt="" />
-                        Source code
-                    </a>
-                    <a className='p-2 border border-white/20 rounded-md flex gap-2 items-center ' href={playPage}>
-                        <img src={play} width='16px' alt="" />
-                        Google Play
-                    </a>
+                    {
+                        source && (
+                            <a className='p-2 border border-white/20 rounded-md flex gap-2 items-center ' href={source}>
+                                <img src={github} width='16px' alt="" />
+                                <span className='w-0 md:w-auto overflow-hidden'>Source code</span>
+                            </a>
+                        )
+                    }
+                    {
+                        playPage && (
+                            <a className='p-2 border border-white/20 rounded-md flex gap-2 items-center ' href={playPage}>
+                                <img src={play} width='16px' alt="" />
+                                <span className='w-0 md:w-auto overflow-hidden'>Google Play</span>
+                            </a>
+                        )
+                    }
                 </div>
             </div>
             <p>
                 {desc}
             </p>
+            <div className="flex gap-2 flex-wrap">
+                {
+                    instrumentals?.map(instrumental => (
+                        <div className="border px-2 py-1 text-xs rounded-md border-slate-400/30 text-slate-500"> {instrumental} </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
